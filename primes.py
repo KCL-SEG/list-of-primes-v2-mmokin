62d1141f675a824487d15d0f9129445be8210fa8
Michael (Michael) Alelegn <michael.alelegn@kcl.ac.uk>
2023-12-10 18:20:51 +0000

    attempt 1

--- primes.py
+++ primes.py
@@ -2,5 +2,16 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes < 1:
+        raise ValueError()
     list = []
+    num = 2
+    while len(list) < number_of_primes:
+        for i in range(2, num):
+            if num % i == 0:
+                break
+        else:
+            list.append(num)
+        num += 1
+
     return list
